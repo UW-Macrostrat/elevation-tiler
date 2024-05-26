82dac4288f147ca2f42edfdd46e1006902b813d8
Daven Quinn <code@davenquinn.com>
2024-05-26 04:41:27 -0500

    A pretty basic proxy tileserver

--- elevation_tiler/__init__.py
+++ elevation_tiler/__init__.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Response
 import httpx
 import os
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
@@ -22,13 +23,22 @@ async def get_tile(request: Request, z: int, x: int, y: int):
 
             # Pass query parameters to the upstream server
             params = dict(request.query_params)
-            response = await client.get(tile_url, headers=headers, params=params)
+            response = await client.get(tile_url, headers={}, params=params)
             response.raise_for_status()
 
-            return response.content
+            return Response(content=response.content, headers=response.headers)
 
     except httpx.HTTPError:
         raise HTTPException(status_code=404, detail="Tile not found")
+
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
