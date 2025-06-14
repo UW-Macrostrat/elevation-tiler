006bae3635ec1a00bc22d33f5323a79a1057c873
Daven Quinn <code@davenquinn.com>
2025-06-14 02:59:52 -0500

    Move towards using query parameters

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 A prototype tile server that can overlay a local COG on a proxied global layer.
 
-Right now, this is configured with environment variables:
-```bash
-# Local or cloud-stored COG to overlay
-OVERLAY_DATASET=data/S23982E015994_S24407E016361_UM_DSM.tif
-```
-
 Eventually, we will extend and integrate this capability with other
 tools.
+
+This is configured to work with a local or remotely stored COG and a global
+layer which will be used to fill in data gaps.
+
+# Using the service
+
+- Provide a COG URL using the `x-overlay-layer` query parameter.
+- Provide a global overlay URL using the `x-fallback-layer` query parameter.
+
+The fallback layer defaults to Mapbox Terrain DEM if not specified, or it can be set
+usign the `FALLBACK_LAYER` environment variable.
+The suffix `{z}/{x}/{y}.{ext}` is appended to layer URLs to fetch tiles.
+
+# TODO
+
+- Integrate with JSON Web Token (JWT) authentication
+- More flexibility in service configuration
+- More options for image processing
+- Serve COG footprints
--- elevation_tiler/__init__.py
+++ elevation_tiler/__init__.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException, Request, Response
 from fastapi.responses import RedirectResponse
 import httpx
-import os
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 from rio_tiler.errors import TileOutsideBounds
@@ -12,18 +11,20 @@ import numpy as N
 from contextvars import ContextVar
 from typing import Optional
 from logging import getLogger
+from os import environ
 
 from .cog_layer import get_raster_tile
 
 load_dotenv()
 
+FALLBACK_LAYER=environ.get("FALLBACK_LAYER", "https://api.mapbox.com/raster/v1/mapbox.mapbox-terrain-dem-v1")
+
+
 log = getLogger("uvicorn.error")
 
 app = FastAPI()
-default_overlay_dataset = os.environ.get("OVERLAY_DATASET", None)
 
 _tilesize = ContextVar("tilesize", default=None)
-
 
 @app.get("/tiles/{z}/{x}/{y}")
 @app.get("/tiles/{z}/{x}/{y}.{ext}")
@@ -31,18 +32,14 @@ async def get_tile(request: Request, z: int, x: int, y: int, ext: Optional[str] 
     # Parse required query parameters
     params = dict(request.query_params)
 
-    tile_url = params.pop("x-fallback-url")
+    fallback_url = params.pop("x-fallback-layer", FALLBACK_LAYER)
+    overlay_layer = params.pop("x-overlay-layer")
 
-    overlay_dataset = params.pop("x-overlay-dataset", default_overlay_dataset)
-
-    if not overlay_dataset:
-        raise HTTPException(status_code=400, detail="No overlay dataset provided")
-
-    tile_url += f"/{z}/{x}/{y}"
+    fallback_url += f"/{z}/{x}/{y}"
     if ext:
-        tile_url += f".{ext}"
+        fallback_url += f".{ext}"
 
-    tile_url = get_base_url(tile_url, params)
+    fallback_url = get_base_url(fallback_url, params)
 
     base = None # The base tile
     overlay = None # The overlay tile
@@ -50,7 +47,7 @@ async def get_tile(request: Request, z: int, x: int, y: int, ext: Optional[str] 
     tilesize = _tilesize.get()
     if tilesize is None:
         # We haven't set the tilesize yet, so we need to load a tile to get the size
-        base = await get_base_tile(tile_url)
+        base = await get_base_tile(fallback_url)
         # Set the tilesize from this request
         _img = create_image_from_bytes(base.content)
         _tilesize.set(_img.array.shape[1])
@@ -59,8 +56,8 @@ async def get_tile(request: Request, z: int, x: int, y: int, ext: Optional[str] 
 
     try:
         # This is where we could add advanced logic for overlaying multiple datasets
-        log.debug("Subsetting overlay tile from: %s", overlay_dataset)
-        overlay = get_raster_tile(overlay_dataset, z, x, y, tilesize=tilesize)
+        log.debug("Subsetting overlay tile from: %s", overlay_layer)
+        overlay = get_raster_tile(overlay_layer, z, x, y, tilesize=tilesize)
     except TileOutsideBounds:
         pass
 
@@ -70,13 +67,13 @@ async def get_tile(request: Request, z: int, x: int, y: int, ext: Optional[str] 
 
     if overlay is None:
         # There is no overlay content, so just proxy the base tile
-        log.debug("Redirecting to base tile: %s", tile_url)
-        return RedirectResponse(url=tile_url, status_code=307)
+        log.debug("Redirecting to base tile: %s", fallback_url)
+        return RedirectResponse(url=fallback_url, status_code=307)
 
     # Here, we have both a base tile and an overlay tile that we need to merge.
     # Get the base tile if we haven't already
     if base is None:
-        base = await get_base_tile(tile_url)
+        base = await get_base_tile(fallback_url)
 
     # Create a merged overlay image
     # Merge the base image with the overlay
@@ -96,7 +93,7 @@ def get_base_url(url: str, params: dict) -> str:
 async def get_base_tile(url: str) -> Response:
     try:
         async with httpx.AsyncClient() as client:
-            log.info("Fetching base tile: %s", url)
+            log.debug("Fetching base tile: %s", url)
             response = await client.get(url)
             response.raise_for_status()
             return response
@@ -115,25 +112,8 @@ def create_image_from_bytes(_bytes: bytes) -> ImageData:
 
 
 def merge_base_image_with_overlay(base: ImageData, overlay: ImageData) -> ImageData:
-    # Get the from the COG with overlay
-
-    # Fill the masked value with the base image
-
-    # Create a constant-valued array with the same shape as the base image
-
-    log.info("Merging base image with overlay, shapes: %s, %s", base.array.shape, overlay.array.shape)
-
+    """Merge the overlay dataset onto the base dataset."""
     assert N.allclose(overlay.array.shape, base.array.shape)
-
-    ##print(overlay.array)
-    ##_array = overlay.array
-
-    # Fill the masked value with zeros
-    # _array = overlay.array.filled(0)
-
-    # expand_mask = N.stack([overlay.array.mask] * 3, axis=-1)
-    # assert expand_mask.shape == base.array.shape
-    # assert expand_mask.shape == overlay.array.shape
 
     arr = base.array.data
     mask = overlay.array.mask
